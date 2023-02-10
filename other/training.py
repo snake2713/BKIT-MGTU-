5d94cceaa85d7a3ee40f5357408afaaf4510f7f9
snake2713 <san.solopow@yandex.ru>
2023-02-10 18:14:58 +0300

    1

---
+++ other/training.py
@@ -0,0 +1,15 @@
+# test = 5 test2 = "Abraham" #str number = 5 #intfnumber = 5.7 #float status  = True 
+# print ("Что нужно вывести") print (555) name = "Abraham" print (name) print ("Он \"плохой\" человек") #экранирование print ('Привет \nмир')
+#конкатенация
+# name = "Sasha" age = 20 print("Привет,меня зовут", name) print ("Привет, мне " + str(age) + " лет ")
+#input 
+# name = input(' Введите свое имя:') age = input(' Сколько вам лет?') print("Привет,",name, "Тебе уже целых",age, "лет", "Круто!")
+# + - и тд
+# a = 5b = 10 c = a + bprint(c)a = 10 a = - aa = - aprint(a)
+#import math 
+# a = 5.6 print(round(a)) print(math.floor(a)) print(math.ceil(a)) print(math.pi) 
+
+# Калькулятор v1
+
+# what = input( "Что делать ?(+ -) " ) a = float(input(" Введи первое число ")) b = float(input ( "Введи второе число " )) if what == "+" : c =a +     print ('результат равен ', c)elif what == "-" :   print ('результат равен ', c)
+# else: print("Я не могу выполнить такую команду")
