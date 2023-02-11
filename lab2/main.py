86c81e6498f22029c2ebfcdac74b1198d436fdcf
snake2713 <san.solopow@yandex.ru>
2023-02-11 13:27:05 +0300

    /

--- lab2/main.py
+++ lab2/main.py
@@ -5,9 +5,9 @@ from lab_python_oop.square import Square
 
 
 def main():
-    r = Rectangle("синего", 7, 8)
-    c = Circle("зеленого", 10)
-    s = Square("красного", 5)
+    r = Rectangle("синего", 15, 15)
+    c = Circle("зеленого", 15)
+    s = Square("красного", 15)
     print(r)
     print(c)
     print(s)
